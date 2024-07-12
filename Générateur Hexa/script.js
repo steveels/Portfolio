9b44f22486bdac505c96880f80ff380703553a5d
steve <steve.elsens@hotmail.com>
2024-07-12 20:43:19 +0200

    update

--- Générateur Hexa/script.js
+++ Générateur Hexa/script.js
@@ -1,22 +1,4 @@
-//   Votre mission est de coder un générateur de dégradés.
-//   Vous allez manipuler des inputs de couleurs afin de créer des "linear-gradient" à la volée !
 
-// A. Coder une interface basique
-// Codez d'abord une interface très simple, contenant les éléments importants : boutons, inputs, liens, etc...
-// Rajoutez un peu de style si besoin est. 
-
-// Puis codez les fonctionnalités JavaScript.
-
-// B. Fonctionnalités JavaScript à coder pour ce projet
-
-// 1. Gérez l'implémentation de base des couleurs, il faut qu'il y est un dégradé lorsqu'on arrive sur le site (input, orientation, body...).
-// 2. Gérez le changement de couleur, on doit pouvoir manipuler les inputs et provoquer le changement de couleur du site.
-// 3. Occupez-vous de l'inclinaison avec l'input type "range".
-// 4. Mettez en place la copie du dégradé en cliquant su le bouton "Copier le gradient".
-// 5. Faites-en sorte de créer des dégradés au hasard en cliquant sur le bouton "random".
-// 6. Bonne chance ! 
-
-// C. Ajoutez du style à l'interface afin de terminer le projet.
 const clr1 = document.getElementById("clr1");
 const clr2= document.getElementById("clr2");
 const r = document.getElementById("r");
@@ -43,8 +25,6 @@ function bgset() {
   code2.textContent =` ${clr2.value}`;
   degre.textContent =`Orientation : ${r.value}°`;
  
-
- 
   
 };
 
@@ -67,13 +47,3 @@ function cp() {
         copy.classList.remove("active");
     },1500);
 }
-
-
-
-
-
-
-
-
-
-
